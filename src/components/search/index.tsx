e3b75a472714d6128e7e5361aa4f142c245728a6
Joao Barros <joao_barross@hotmail.com>
2025-04-03 17:58:41 -0300

    adiciona novos ícones e aprimora a interface de busca com imagens e layout responsivo

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,63 +1,101 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
-import '@ant-design/v5-patch-for-react-19';
-import { Button, Flex, Input } from "antd"
+import "@ant-design/v5-patch-for-react-19";
+import { Button, Flex, Input } from "antd";
 import { useState } from "react";
 import { isValidSearch } from "./helper";
-import { fetchNews } from '@/helpers/api';
-import { notify } from '../../helpers/Notification';
-import { NewsItem } from '@/pages/api/fetch-news';
-import { useCurrentMenuNewsContext } from '@/context/current-menu-news';
+import { fetchNews } from "@/helpers/api";
+import { notify } from "../../helpers/Notification";
+import { NewsItem } from "@/pages/api/fetch-news";
+import { useCurrentMenuNewsContext } from "@/context/current-menu-news";
+import Image from "next/image";
 
 export const SearchComponent = () => {
-    const { handleAddMenuItems } = useCurrentMenuNewsContext()
-    const [search, setSearch] = useState<string>("");
-    const [recomendations, setRecomendations] = useState<string[]>([
-        'Música',
-        'Vídeo Games',
-        'Empreendedorismo',
-        'Café',
-    ])
+  const { handleAddMenuItems } = useCurrentMenuNewsContext();
+  const [search, setSearch] = useState<string>("");
+  const [recomendations, setRecomendations] = useState<string[]>([
+    "Música",
+    "Vídeo Games",
+    "Empreendedorismo",
+    "Café",
+  ]);
 
-    const handleContent = (title: string, content: NewsItem[]) => {
-        handleAddMenuItems({
-            content,
-            label: title
-        })
-    }
+  const handleContent = (title: string, content: NewsItem[]) => {
+    handleAddMenuItems({
+      content,
+      label: title,
+    });
+  };
 
-    const fetchSearch = async (search: string) => {
-        await fetchNews(search)
-            .then(res => {
-                notify({ message: 'Sucesso!', duration: 4.5, placement: 'topRight', type: 'success' });
-                handleContent(search, res.content)
-            })
-            .catch(err => {
-                notify({ message: 'Houve um problema...', description: err.message, duration: 4.5, placement: 'topRight', type: 'error' });
-            })
-    }
+  const fetchSearch = async (search: string) => {
+    await fetchNews(search)
+      .then((res) => {
+        notify({
+          message: "Sucesso!",
+          duration: 4.5,
+          placement: "topRight",
+          type: "success",
+        });
+        handleContent(search, res.content);
+      })
+      .catch((err) => {
+        notify({
+          message: "Houve um problema...",
+          description: err.message,
+          duration: 4.5,
+          placement: "topRight",
+          type: "error",
+        });
+      });
+  };
 
-    const handleSearchChange = (text: string) => {
-        isValidSearch(text) && fetchSearch(text)
-    };
+  const handleSearchChange = (text: string) => {
+    isValidSearch(text) && fetchSearch(text);
+  };
 
-    return (
-        <Flex style={{
-            flexDirection: 'column',
-            gap: '5px',
-            padding: '10px'
-        }}>
-            <Input placeholder="Procure aqui um assunto do seu interesse" onChange={(e) => setSearch(e.target.value)}></Input>
-            <Flex style={{
-                gap: '5px',
-                justifyContent: 'center'
-            }}>
-                {
-                    !!recomendations && recomendations.map((item, i) => (
-                        <Button key={i} onClick={() => handleSearchChange(item)}>{item}</Button>
-                    ))
-                }
-            </Flex>
-            <Button type="primary" onClick={() => handleSearchChange(search)}>Achar notícias!</Button>
+  return (
+    <>
+      <img
+        src="/search.png"
+        alt="Logo"
+        style={{ width: "200px", height: "auto" }}
+      />
+      <h1
+        style={{
+          fontSize: "2rem",
+          fontWeight: "bold",
+        }}
+      >
+        Busque por notícias de seu interesse!
+      </h1>
+      <Flex
+        style={{
+          flexDirection: "column",
+          gap: "5px",
+          padding: "10px",
+        }}
+      >
+        <Input
+          placeholder="Procure aqui um assunto do seu interesse"
+          onChange={(e) => setSearch(e.target.value)}
+        ></Input>
+        <Flex
+          style={{
+            gap: "5px",
+            justifyContent: "center",
+          }}
+        >
+          {!!recomendations &&
+            recomendations.map((item, i) => (
+              <Button key={i} onClick={() => handleSearchChange(item)}>
+                {item}
+              </Button>
+            ))}
         </Flex>
-    )
-}
+        <Button type="primary" onClick={() => handleSearchChange(search)}>
+          Achar notícias!
+        </Button>
+      </Flex>
+    </>
+  );
+};
